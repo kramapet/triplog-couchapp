b7b114b44149058d4b2f0b8a744d507ba4d21778
Petr Kramar <kramar@masquerade.cz>
2014-03-11 08:38:37 +0100

    deleted semicolon

--- _attachments/scripts/services.js
+++ _attachments/scripts/services.js
@@ -6,7 +6,7 @@ angular.module('triplogApp.services', ['ngResource'])
 	
 	.factory('ShowdownConverter', ['$window', function ($window) {
 		return new $window.Showdown.converter();
-	}]);
+	}])
 
 	.factory('Posts', ['$resource', function ($resource) {
 		return $resource('/posts', {}, {
