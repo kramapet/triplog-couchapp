cfc385458746b4e8c427a610fff0a087f1bff685
Petr Kramar <kramar@masquerade.cz>
2014-03-11 08:31:02 +0100

    add filter to convert text by Showdown

--- _attachments/scripts/filters.js
+++ _attachments/scripts/filters.js
@@ -3,6 +3,12 @@
 /* filters */
 
 angular.module('triplogApp.filters', [])
+	.filter('showdown', ['ShowdownConverter', function (ShowdownConverter) {
+		return function (text) {
+			return ShowdownConverter.makeHtml(text);
+		}
+	}])
+
 	.filter('arrdate2epochtime', [function () {
 		return function (arr) {
 			return new Date(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]).getTime();
