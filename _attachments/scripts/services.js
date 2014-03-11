9622c629f6149d7c18405ca3efaf2969dbde83ea
Petr Kramar <kramar@masquerade.cz>
2014-03-11 08:32:03 +0100

    add 'ShowdownConverter' service

--- _attachments/scripts/services.js
+++ _attachments/scripts/services.js
@@ -3,6 +3,10 @@
 /* services */
 
 angular.module('triplogApp.services', ['ngResource'])
+	
+	.factory('ShowdownConverter', ['$window', function ($window) {
+		return new $window.Showdown.converter();
+	}]);
 
 	.factory('Posts', ['$resource', function ($resource) {
 		return $resource('/posts', {}, {
