477ebce9421969638c387fac5c9380ff07c12888
Petr Kramar <kramar@masquerade.cz>
2014-03-17 11:21:27 +0100

    add new map function to index data by url

---
+++ views/objects_by_url/map.js
@@ -0,0 +1,20 @@
+function (doc) {
+	obj = {
+		'class': doc.class,
+		'title': doc.title,
+		'url': doc.url,
+		'body': doc.body,
+		'published_at': doc.published_at,
+	};
+
+	switch (doc.class) {
+		case 'route':
+			obj.geo = doc.geo;
+			break;
+		case 'photo':
+			obj.photoDescription = doc.photoDescription;
+			break;
+	}
+
+	emit(doc.url, obj);
+}
