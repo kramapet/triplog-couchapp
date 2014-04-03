a853ec1701d89a7a6a563b7e49e526a4f46d44c4
Petr Kramar <kramar@masquerade.cz>
2014-04-03 16:31:54 +0200

    moved function get_end_start_key to services

--- _attachments/scripts/controllers.js
+++ _attachments/scripts/controllers.js
@@ -2,33 +2,17 @@
 
 /* controllers */
 angular.module('triplogApp.controllers', [])
-	.controller('MainController', ['$scope', '$routeParams', 'Objects', function ($scope, $routeParams, Objects) {
-		function get_end_start_key(year, month) {
-		 	var endkey = [year, month, 0, 0, 0, 0];
-			if (month == 11) {
-				year++;
-				month = 0;
-			} else {
-				month++;
-			}
-			var startkey = [year, month, 0, 0, 0, 0];
-
-			return {
-				'startkey': '[' + startkey + ']', 
-				'endkey': '['+ endkey + ']'
-			};
-		}
-
+	.controller('MainController', ['$scope', '$routeParams', 'Objects', 'GetKeysForMonth', function ($scope, $routeParams, Objects, GetKeysForMonth) {
 		if ($routeParams.year == undefined) {
 			var last_page = Objects.queryLastPage({}, function () {
 				var year = last_page.rows[0].key[0];
 				var month = last_page.rows[0].key[1];
 
-				var keys = get_end_start_key(year, month);
+				var keys = GetKeysForMonth(year, month);
 				$scope.objects = Objects.query(keys);
 			});
 		} else {
-			$scope.objects = Objects.query(get_end_start_key($routeParams.year, $routeParams.month));
+			$scope.objects = Objects.query(GetKeysForMonth($routeParams.year, $routeParams.month));
 		}
 
 
