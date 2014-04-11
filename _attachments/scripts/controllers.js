8326cdb281df6e484f29a5ed08a6f560230ccb96
petr <petr@kramar.local>
2014-04-11 23:59:56 +0200

    update AdminController
    AdminController has been update to be able to work
    with CouchDB service which is returning a promise now

--- _attachments/scripts/controllers.js
+++ _attachments/scripts/controllers.js
@@ -38,50 +38,52 @@ angular.module('triplogApp.controllers', [])
 			$scope.errordata = "Oh, snap! " + payload.reason;
 		};
 		
-		$scope.detail = CouchDB.db.newDoc();
-		if (!$routeParams.id) {
-			$scope.action = 'add';
-			$scope.detail.class = 'post';
-		} else {
-			$scope.action = 'edit';
-			$scope.detail.load($routeParams.id).success(function () {
-				console.log(arguments);
-			}).error(function () {
-				console.log(arguments);
-			});
-		}
-
-		$scope.update = function () { 
-			if (!$scope.detail._id && $scope.detail.url) {
-				$scope.detail._id = $scope.detail.url;
-			}
-
-			if (Object.prototype.toString.call($scope.detail.published_at) !== '[object Array]') {
-				$scope.detail.published_at = $scope.detail.published_at.split(",");
-				for (var i in $scope.detail.published_at) {
-					$scope.detail.published_at[i] = parseInt($scope.detail.published_at[i]);
-				}
-			}
+		CouchDB.then(function (couch) {
+			$scope.detail = couch.db.newDoc();
 			
-			$scope.detail.save().success(function () {
-				$scope.errordata = null;
-				$scope.okdata = 'Document has been saved';
-			}).error(setError);
-		};
+			if (!$routeParams.id) {
+				$scope.action = 'add';
+				$scope.detail.class = 'post';
+			} else {
+				$scope.action = 'edit';
+				$scope.detail.load($routeParams.id).success(function () {
+					console.log(arguments);
+				}).error(function () {
+					console.log(arguments);
+				});
+			}
 
-		$scope.destroy = function () {
-			$scope.detail.remove().success(function () {
-				$scope.errordata = null;
-				$scope.okdata = "Document has been removed";
-				$scope.detail = CouchDB.db.newDoc();
-			}).error(setError);
-		};
+			$scope.update = function () {
+				if (!$scope.detail._id && $scope.detail.url) {
+					$scope.detail._id = $scope.detail.url;
+				}
 
-		$scope.attach = function (id) {
-			var fileInput = document.getElementById(id);
-			$scope.detail.attachMulti(fileInput.files, function () {
-				fileInput.value = "";
-			});
-		};
+				if (!angular.isArray($scope.detail.published_at)) {
+					$scope.detail.published_at = $scope.detail.published_at.split(",");
+					for (var i in $scope.detail.published_at) {
+						$scope.detail.published_at[i] = parseInt($scope.detail.published_at[i], 10);
+					}
+				}
 
+				$scope.detail.save().success(function () {
+					$scope.errordata = null;
+					$scope.okdata = 'Document has been saved';
+				}).error(setError);
+			};
+
+			$scope.destroy = function () {
+				$scope.detail.remove().success(function () {
+					$scope.errordata = null;
+					$scope.okdata = 'Document has been removed';
+					$scope.detail = couch.db.newDoc();
+				}).error(setError);
+			};
+
+			$scope.attach = function (id) {
+				var fileInput = document.getElementById(id);
+				$scope.detail.attachMulti(fileInput.files, function () {
+					fileInput.value = "";
+				});
+			};
+		});
 	}]);
