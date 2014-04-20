f4800dc0421d38c95bc6f8719622da5486beb69a
Petr Kramar <kramar@masquerade.cz>
2014-04-20 10:27:06 +0200

    show MainController only for authenticated users with read rights

--- _attachments/scripts/controllers.js
+++ _attachments/scripts/controllers.js
@@ -2,23 +2,29 @@
 
 /* controllers */
 angular.module('triplogApp.controllers', [])
-	.controller('MainController', ['$scope', '$routeParams', 'Objects', 'GetKeysForMonth', 'EmptyQuery', 'Config', function ($scope, $routeParams, Objects, GetKeysForMonth, EmptyQuery, Config) {
-		if ($routeParams.year == undefined) {
-			var last_page = Objects.queryLastPage({}, function () {
-				if (last_page.rows.length == 0) {
-					$scope.objects = EmptyQuery();
-				} else {
-					var year = last_page.rows[0].key[0];
-					var month = last_page.rows[0].key[1];
-					var keys = GetKeysForMonth(year, month);
-					$scope.objects = Objects.query(keys);
-				}
-			});
-		} else {
-			$scope.objects = Objects.query(GetKeysForMonth($routeParams.year, $routeParams.month));
-		}
+	.controller('MainController', ['$scope', '$routeParams', 'Objects', 'GetKeysForMonth', 'EmptyQuery', 'Config', 'HasRole', '$location', function ($scope, $routeParams, Objects, GetKeysForMonth, EmptyQuery, Config, HasRole, $location) {
+		var success_cb = function () {
+			if ($routeParams.year == undefined) {
+				var last_page = Objects.queryLastPage({}, function () {
+					if (last_page.rows.length == 0) {
+						$scope.objects = EmptyQuery();
+					} else {
+						var year = last_page.rows[0].key[0];
+						var month = last_page.rows[0].key[1];
+						var keys = GetKeysForMonth(year, month);
+						$scope.objects = Objects.query(keys);
+					}
+				});
+			} else {
+				$scope.objects = Objects.query(GetKeysForMonth($routeParams.year, $routeParams.month));
+			}
+		};
 
-		$scope.config = Config.query();
+		var error_cb = function () {
+			$location.path('/error404');
+		};
+	
+		HasRole('triplog-reader').then(success_cb, error_cb);
 	}])
 
 	.controller('DetailController', ['$scope', '$routeParams', 'ObjectsByUrl', function ($scope, $routeParams, ObjectsByUrl) {
