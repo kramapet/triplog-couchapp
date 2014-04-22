9660dacf48cf35c161ed7fe27c93ded020c8f740
Petr Kramar <kramar@masquerade.cz>
2014-04-22 15:54:06 +0200

    apply roles for controllers, delete LoginController

--- _attachments/scripts/controllers.js
+++ _attachments/scripts/controllers.js
@@ -27,41 +27,27 @@ angular.module('triplogApp.controllers', [])
 		HasRole('triplog-reader').then(success_cb, error_cb);
 	}])
 
-	.controller('DetailController', ['$scope', '$routeParams', 'ObjectsByUrl', function ($scope, $routeParams, ObjectsByUrl) {
-		$scope.objects = ObjectsByUrl.get({ 'url': $routeParams.detailUrl });
+	.controller('DetailController', ['$scope', '$routeParams', 'ObjectsByUrl', 'HasRole', '$location', function ($scope, $routeParams, ObjectsByUrl, HasRole, $location) {
+		HasRole('triplog-reader').then(function () {
+			$scope.objects = ObjectsByUrl.get({ 'url': $routeParams.detailUrl });
+		}, function () {
+			$location.path('/error404');
+		});
 	}])
 
 	.controller('Error404Controller', [function () {
 	}])
 	
-	.controller('BigMapController', ['$scope', 'Routes', function ($scope, Routes) {
-		$scope.routes = Routes.query();
+	.controller('BigMapController', ['$scope', 'Routes', 'HasRole', '$location', function ($scope, Routes, HasRole, $location) {
+		HasRole('triplog-reader').then(function () {
+			$scope.routes = Routes.query();
+		}, function () {
+			$location.path('/error404');
+		});
 	}])
 
-	.controller('LoginController', ['$scope', '$location', 'CouchDB', function ($scope, $location, CouchDB) {
-		$scope.credentials = {
-			'username': '',
-			'password': ''
-		};
-		$scope.message = null;
-
-		var setMessage = function (data) {
-			$scope.message = data.reason;
-		};
-
-		$scope.login = function () {
-			CouchDB.then(function (couch) {
-				couch.server.login($scope.credentials.username, $scope.credentials.password).success(function () {
-					$scope.message = 'User has been authenticated';
-					$scope.credentials.username = $scope.credentials.password = '';
-					$location.path('/home');
-				}).error(setMessage);
-			});
-		};
-	}])
-	
-	.controller('AdminController', ['$scope', '$routeParams', '$location', 'CouchDB', '$window', 
-	function ($scope, $routeParams, $location, CouchDB, $window) {
+	.controller('AdminController', ['$scope', '$routeParams', '$location', 'CouchDB', '$window', 'HasRole',
+	function ($scope, $routeParams, $location, CouchDB, $window, HasRole) {
 		var setError = function (payload) {
 			$scope.okdata = null;
 			$scope.errordata = "Oh, snap! " + payload.reason;
@@ -79,102 +65,110 @@ angular.module('triplogApp.controllers', [])
 			}
 		};
 
+		var success_cb = function () {
 		
-		CouchDB.then(function (couch) {
-			$scope.detail = couch.db.newDoc();
-			
-			if (!$routeParams.id) {
-				$scope.action = 'add';
-				$scope.detail.class = 'post';
-			} else {
-				$scope.action = 'edit';
-				$scope.detail.load($routeParams.id).success(function () {
-					refreshPhotoDescription($scope.detail);
-				}).error(setError);
-			}
-
-			$scope.update = function () {
-				if (!$scope.detail._id && $scope.detail.url) {
-					$scope.detail._id = $scope.detail.url;
+			CouchDB.then(function (couch) {
+				$scope.detail = couch.db.newDoc();
+				
+				if (!$routeParams.id) {
+					$scope.action = 'add';
+					$scope.detail.class = 'post';
+				} else {
+					$scope.action = 'edit';
+					$scope.detail.load($routeParams.id).success(function () {
+						refreshPhotoDescription($scope.detail);
+					}).error(setError);
 				}
 
-				if (!angular.isArray($scope.detail.published_at)) {
-					$scope.detail.published_at = $scope.detail.published_at.split(",");
-					for (var i in $scope.detail.published_at) {
-						$scope.detail.published_at[i] = parseInt($scope.detail.published_at[i], 10);
+				$scope.update = function () {
+					if (!$scope.detail._id && $scope.detail.url) {
+						$scope.detail._id = $scope.detail.url;
 					}
-				}
 
-				$scope.detail.save().success(function () {
-					$scope.errordata = null;
-					$scope.okdata = 'Document has been saved';
-				}).error(setError);
-			};
-
-			$scope.destroy = function () {
-				$scope.detail.remove().success(function () {
-					$scope.errordata = null;
-					$scope.okdata = 'Document has been removed';
-					$scope.detail = couch.db.newDoc();
-				}).error(setError);
-			};
-
-			$scope.attach = function (idx) {
-				var fileInput = document.getElementById(idx);
-				$scope.detail.attachMulti(fileInput.files, function () {
-					fileInput.value = "";
-					$scope.okdata = 'Files have been uploaded.'
-					$scope.errordata = null;
-					refreshPhotoDescription($scope.detail);
-				});
-			};
-
-			$scope.detach = function (name) {
-				$scope.detail.detach(name);
-				if ($scope.detail.photoDescription[name]) {
-					delete $scope.detail.photoDescription[name];
-				}
-			};
-
-			$scope.processGPX = function (idx) {
-				var fileInput = document.getElementById(idx);
-				var file = fileInput.files[0];
-				if (!file.type.match(/gpx\+xml/)) {
-					setError({'reason': 'Invalid format'});
-					return;
-				}
-			
-				var parser = new $window.DOMParser();
-				var reader = new FileReader();
-				reader.onload = (function (f) {
-					return function (event) {
-						var doc = parser.parseFromString(event.target.result, 'application/xml');
-						var points = doc.getElementsByTagName('trkpt');
-						if (points.length > 0) {
-							$scope.detail.geo = {
-								'type': 'Feature',
-								'geometry': {
-									'type': 'LineString',
-									'coordinates': []
-								}
-							};
+					if (!angular.isArray($scope.detail.published_at)) {
+						$scope.detail.published_at = $scope.detail.published_at.split(",");
+						for (var i in $scope.detail.published_at) {
+							$scope.detail.published_at[i] = parseInt($scope.detail.published_at[i], 10);
 						}
+					}
 
-						for (var i in points) {
-							if (points[i].attributes && points[i].attributes['lon'] && points[i].attributes['lat']) {
-								$scope.detail.geo.geometry.coordinates.push([ 
-									points[i].attributes['lon'].nodeValue, 
-									points[i].attributes['lat'].nodeValue 
-								]);
+					$scope.detail.save().success(function () {
+						$scope.errordata = null;
+						$scope.okdata = 'Document has been saved';
+					}).error(setError);
+				};
+
+				$scope.destroy = function () {
+					$scope.detail.remove().success(function () {
+						$scope.errordata = null;
+						$scope.okdata = 'Document has been removed';
+						$scope.detail = couch.db.newDoc();
+					}).error(setError);
+				};
+
+				$scope.attach = function (idx) {
+					var fileInput = document.getElementById(idx);
+					$scope.detail.attachMulti(fileInput.files, function () {
+						fileInput.value = "";
+						$scope.okdata = 'Files have been uploaded.'
+						$scope.errordata = null;
+						refreshPhotoDescription($scope.detail);
+					});
+				};
+
+				$scope.detach = function (name) {
+					$scope.detail.detach(name);
+					if ($scope.detail.photoDescription[name]) {
+						delete $scope.detail.photoDescription[name];
+					}
+				};
+
+				$scope.processGPX = function (idx) {
+					var fileInput = document.getElementById(idx);
+					var file = fileInput.files[0];
+					if (!file.type.match(/gpx\+xml/)) {
+						setError({'reason': 'Invalid format'});
+						return;
+					}
+				
+					var parser = new $window.DOMParser();
+					var reader = new FileReader();
+					reader.onload = (function (f) {
+						return function (event) {
+							var doc = parser.parseFromString(event.target.result, 'application/xml');
+							var points = doc.getElementsByTagName('trkpt');
+							if (points.length > 0) {
+								$scope.detail.geo = {
+									'type': 'Feature',
+									'geometry': {
+										'type': 'LineString',
+										'coordinates': []
+									}
+								};
 							}
-						}
 
-						$scope.update();
-					};
-				})(file);
+							for (var i in points) {
+								if (points[i].attributes && points[i].attributes['lon'] && points[i].attributes['lat']) {
+									$scope.detail.geo.geometry.coordinates.push([ 
+										points[i].attributes['lon'].nodeValue, 
+										points[i].attributes['lat'].nodeValue 
+									]);
+								}
+							}
 
-				reader.readAsText(file);
-			};
+							$scope.update();
+						};
+					})(file);
 
+					reader.readAsText(file);
+				};
+
+			});
+
+		};
+
+		HasRole('triplog-writer').then(success_cb, function () {
+			$location.path('/error404');
 		});
+
 	}]);
